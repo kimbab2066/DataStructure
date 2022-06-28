5cdf34e113585f2422cc4fbe4300db11ed03b88c
kimbab2066 <qseft523125@g.yju.ac.kr>
2022-06-28 23:49:41 +0900

    Graph

---
+++ DataStructure/src/Graph/test.java
@@ -0,0 +1,29 @@
+package Graph;
+
+public class test {
+	public static void main(String[] args) {
+		Graph<Integer, Integer> g = new GraphListDirected<>();
+		g.add(1);
+		g.add(2);
+		g.add(10);
+		g.add(5);
+		g.add(7);
+		g.add(17);
+		g.add(21);
+		g.add(51);
+
+		g.addEdge(1, 2, null);
+		g.addEdge(1, 10, null);
+		g.addEdge(2, 10, null);
+		g.addEdge(2, 5, null);
+		g.addEdge(5, 17, null);
+		g.addEdge(10, 7, null);
+		g.addEdge(5, 21, null);
+		g.addEdge(5, 51, null);
+		g.addEdge(7, 51, null);
+
+		g.DFS(1);
+		g.BFS(1);
+		System.out.println(g);
+	}
+}
